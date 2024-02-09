5c04b0b363a4771f667b65b4efb08e7efae3d633
Il tuo nome <svilupposoftware2023@a24pc04.studenti.engim.it>
2024-02-09 13:20:51 +0100

    Tentativo fallimentare creazione oggetto con metodo

---
+++ LezioneSergio/main.js
@@ -0,0 +1,15 @@
+/* Fare un calcolatrice tra due numeri con addizione, sottrazione, moltiplicazione */
+
+function addizione(a,b){
+    return a+b;
+} 
+function sottrazione(a,b){
+    return a-b;
+}
+function moltiplicazione(a,b){
+    return a*b;
+}
+
+console.log(addizione(2,3))
+console.log(sottrazione(2,3))
+console.log(moltiplicazione(2,3))
