baa2940ad4772916bf363c6ec427832c0620e6c9
Il tuo nome <svilupposoftware2023@a24pc04.studenti.engim.it>
2024-02-22 13:18:08 +0100

    Esercizi in classe e impostazione battaglia navale

---
+++ battagliaNavale.js
@@ -0,0 +1,45 @@
+// battaglia navale
+
+//FE html
+
+//il comportamento in js
+
+//ingredienti
+
+//FE
+//2 griglie campo da gioco formati da x righe e x colonne
+//implementate con div o table
+//dove visualizzare i colpi effettuati 
+//visualizzo:
+//  bianco -> nessun colpo
+//  grigio -> acqua
+//  rosso  -> colpito
+
+//due campi testo che accettano solo interi < x in cui il giocatore inserisce le coordinate di giocata
+//un bottone che chiama la funzione colpisci
+
+//codice
+//gli array di posizionamento li creiamo lato codice, senza interazione
+//un array tridimensionale, due elementi vhe contengono un array bidimensionale di x righe e x colonne per il posizionamento delle navi
+//navi posizionate, array int (0 -> nessuna nave, 1 -> nave posizionata)
+let campi = [
+[
+    [
+        []
+    ]
+]//player1 
+,
+[
+    [
+        []
+    ]
+]//player2
+]
+
+let naviColpite = [0,0]
+
+//funzione colpisci
+//ha in input il giocatore, la riga e la colonna da colpire
+//colora il campo di gioco dell'avversario discriminando il colore
+//se colpisce incrementa numero navi colpite
+//se numero navi colpite == massimo segnala vittoria
